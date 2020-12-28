da51ae76a485d2213daf84fda19abb5a0eca012e
Black <designintheblack@gmail.com>
2020-12-28 17:09:14 -0600

    Initial React Application Commit for Nota App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>Nota</h2>
+    <p>Nota will be a contact book, containing useful information
+       on friends, colleagues, and relatives in an easy to use web format.
+    </p>
+    </div>
+  );
+}
+
+export default App;
