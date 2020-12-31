62430d821256a506166ce6343b9688cd865183fb
Black <designintheblack@gmail.com>
2020-12-30 19:13:22 -0600

    Set up basic application logic, limited styling, etc. Will need to refactor as components grow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+//Will be used to manage updates to entries
+function Manager() {
+  return (
+    <div>Managing Content</div>
+  );
+}
+
+//Trivial Search Component
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Query: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+//Trivial List Component --> Update me as the data structure changes, please.
+const List = props => {
+ return props.list.map(function(contact) {
+  return (
+   <div key = {contact.contactID}>
+    <ul>
+      <h4>{contact.Name}</h4>
+      <li>Age: {contact.Age}</li>
+      <li>Favorite Food: {contact.FavFood}</li>
+    </ul>
+   </div>
+ );
+})};
+
 
 function App() {
+
+  //Search State Management
+  const [query, setQuery] = React.useState('');
+  const queryUpdate = event => {
+    setQuery(event.target.value);
+  };
+
+  //To be populated with server and database later
+  const dummydata = [
+    {
+      Name: 'James Dulaney',
+      Age: 31,
+      FavFood: "Calzone"
+    },
+    {
+      Name: 'Alana Kraus',
+      Age: 24,
+      FavFood: "French Onion Soup"
+    },
+    {
+      Name: 'Thomas Edison',
+      Age: 110,
+      FavFood: "Freshly Baked Bread"
+    },
+  
+  ];
+
   return (
     <div className="App">
-    <h2>Nota</h2>
-    <p>Nota will be a contact book, containing useful information
-       on friends, colleagues, and relatives in an easy to use web format.
-    </p>
+      <h2>Welcome to Nota</h2>
+      <Search />
+      <hr></hr>
+      <List list = {dummydata}/>
     </div>
   );
 }
